42fc04c02dcdc342f7b9bfbf2c4b92f8f5e30a9e
atherman2 <alexandre.t.herman@gmail.com>
2025-07-06 12:11:43 -0300

    Added '__str__()' method to class Task at 'tss_tasks/task.py'

--- tss_tasks/task.py
+++ tss_tasks/task.py
@@ -7,3 +7,7 @@ class Task:
             self.id = info_list[0]
             self.duration = info_list[1]
             self.priority = info_list[2]
+
+    def __str__(self):
+
+        return f"[Task: id={self.id}, duration={self.duration}, priority={self.priority} ]"
