aa806bfa3bbde2b4127a17a293a1c286542f30c4
atherman2 <alexandre.t.herman@gmail.com>
2025-07-06 11:30:33 -0300

    Task class created. Basic info

---
+++ tss-tasks/task.py
@@ -0,0 +1,9 @@
+class Task:
+
+    def __init__(self, info_list=None):
+        
+        if info_list != None:
+
+            self.id = info_list[0]
+            self.duration = info_list[1]
+            self.priority = info_list[2]
