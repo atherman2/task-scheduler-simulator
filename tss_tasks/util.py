04ba503a80f73f5a6f1f4d9649425c60f09c6d62
atherman2 <alexandre.t.herman@gmail.com>
2025-07-06 11:54:34 -0300

    tss_tasks/util.py created. Includes a function to read task info from a list of strings and return a dictionary with task enter time as key

---
+++ tss_tasks/util.py
@@ -0,0 +1,23 @@
+from tss_tasks.task import *
+
+def task_dict(info_lines: list[str]) -> dict[int, Task]:
+
+    task_dictionary = {}
+    try:
+        for info_line in info_lines:
+                
+            info_line = info_line.split(";")
+                
+            task_id, task_enter_time, task_duration, task_priority = info_line
+            task_enter_time = int(task_enter_time)
+            task_duration = int(task_duration)
+            task_priority = int(task_priority)
+
+            task_info = [task_id, task_duration, task_priority]
+            task_dictionary[task_enter_time] = Task(task_info)
+    except:
+
+        print("\nERROR: The input must be in the format:")
+        print("<task_id:str>;<task_enter_time:int>;<task_duration:int>;<task_priority:int>")
+        return None
+    return task_dictionary
